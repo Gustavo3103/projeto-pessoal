9181df9c0dffe513202dc2cd138194e9581e1284
Gustavo Soares <gu03soares@gmail.com>
2024-04-01 23:36:31 -0300

    Projeto - Operações matemáticas

---
+++ JS/funcaoMatematica.js
@@ -0,0 +1,8 @@
+function funcao() {
+            
+    soma.innerHTML = i1.value + " + " + i2.value + " = " + (Number(i1.value) + Number(i2.value))
+    subtracao.innerHTML = i1.value + " - " + i2.value + " = " + (Number(i1.value) - Number(i2.value))
+    multiplicacao.innerHTML = i1.value + " x " + i2.value + " = " + (Number(i1.value) * Number(i2.value))
+    divisao.innerHTML = i1.value + " ÷ " + i2.value + " = " + (Number(i1.value) / Number(i2.value))
+
+}
